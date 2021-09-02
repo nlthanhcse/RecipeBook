4395b2a85a1d389281bc69479d431b9a07086187
nlthanhcse <nlthanhititiu17038@gmail.com>
2021-09-02 19:58:56 +0700

    Add event for clicking on header elements.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() headerSelected = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onHeaderSelect(headerEle: number) {
+    this.headerSelected.emit(headerEle);
   }
 
 }
