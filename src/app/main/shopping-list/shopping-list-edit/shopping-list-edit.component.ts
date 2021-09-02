25858bad7a917ac0f45cad8228b2b4b0a79b77b3
nlthanhcse <nlthanhititiu17038@gmail.com>
2021-09-02 20:47:28 +0700

    Add event for clicking add ingredient

--- src/app/main/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/main/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @Output() addIngredientEvent = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddIngredient(ingName: HTMLInputElement, inAmount: HTMLInputElement) {
+    const ingredient = new Ingredient(ingName.value, parseInt(inAmount.value));
+    this.addIngredientEvent.emit(ingredient);
+  }
 }
